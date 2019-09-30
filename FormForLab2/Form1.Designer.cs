883546cc31b0c6d7ac0d5dad71bfea70271d761a
r_lakusta <makulatura78@gmail.com>
2019-09-30 14:26:07 +0300

    Добавьте файлы проекта.

---
+++ FormForLab2/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace FormForLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chBCsv = new System.Windows.Forms.CheckBox();
+            this.chBXls = new System.Windows.Forms.CheckBox();
+            this.chBDoc = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(374, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(374, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(374, 205);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(129, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "За замовчуванням розширення встановлюється .csv";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Введіть назву для файлу з даними про дату";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(282, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Введіть назву для файлу з даними про провідомлення";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(344, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Введіть назву для файлу з даними про відправника та отримувача";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(78, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chBDoc);
+            this.groupBox1.Controls.Add(this.chBXls);
+            this.groupBox1.Controls.Add(this.chBCsv);
+            this.groupBox1.Location = new System.Drawing.Point(537, 125);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Виберіть формат файлу";
+            // 
+            // chBCsv
+            // 
+            this.chBCsv.AutoSize = true;
+            this.chBCsv.Location = new System.Drawing.Point(7, 20);
+            this.chBCsv.Name = "chBCsv";
+            this.chBCsv.Size = new System.Drawing.Size(46, 17);
+            this.chBCsv.TabIndex = 0;
+            this.chBCsv.Text = ".csv";
+            this.chBCsv.UseVisualStyleBackColor = true;
+            // 
+            // chBXls
+            // 
+            this.chBXls.AutoSize = true;
+            this.chBXls.Location = new System.Drawing.Point(7, 41);
+            this.chBXls.Name = "chBXls";
+            this.chBXls.Size = new System.Drawing.Size(41, 17);
+            this.chBXls.TabIndex = 1;
+            this.chBXls.Text = ".xls";
+            this.chBXls.UseVisualStyleBackColor = true;
+            // 
+            // chBDoc
+            // 
+            this.chBDoc.AutoSize = true;
+            this.chBDoc.Location = new System.Drawing.Point(7, 64);
+            this.chBDoc.Name = "chBDoc";
+            this.chBDoc.Size = new System.Drawing.Size(47, 17);
+            this.chBDoc.TabIndex = 2;
+            this.chBDoc.Text = ".doc";
+            this.chBDoc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chBDoc;
+        private System.Windows.Forms.CheckBox chBXls;
+        private System.Windows.Forms.CheckBox chBCsv;
+    }
+}
+
---
+++ FormForLab2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using LabWork;
+namespace FormForLab2
+{
+    public partial class Form1 : Form
+    {
+        static string format;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           if(chBCsv.Enabled)
+            {
+                chBDoc.Enabled = false;
+                chBXls.Enabled = false;
+                format = ".csv";
+            }
+           else if(chBDoc.Enabled)
+            {
+                chBCsv.Enabled = false;
+                chBXls.Enabled = false;
+                format = ".doc";
+            }
+           else if(chBXls.Enabled)
+            {
+                chBDoc.Enabled = false;
+                chBCsv.Enabled = false;
+                format = ".xls";
+            }
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+            label1.Text="Ви вибрали файл "+  dialog.FileName.Split('\\').Last();
+            
+            if (textBox1.Text != null && textBox2.Text != null && textBox3.Text != null)
+            {               
+                Main.TryParseFile(dialog.FileName,textBox1.Text,textBox2.Text,textBox3.Text,format);                
+            }
+            else
+            {
+
+            }
+            MessageBox.Show(Main.complete, "Результат", MessageBoxButtons.OKCancel);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.ShowDialog();
+            this.BackgroundImage = Image.FromFile(open.FileName);
+        }
+
+        
+    }
+}
